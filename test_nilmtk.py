8e6aea5c4263e215d116d32347145cfb913d6961
Michael Milicevich <milicevichm@gmail.com>
2015-03-09 13:31:56 -0400

    Changed test script to test total dependancies

--- test_nilmtk.py
+++ test_nilmtk.py
@@ -1,4 +1,12 @@
+# import modules/dependencies
+from __future__ import print_function, division
+import sys
+from dateutil import parser
+import pandas as pd
+from key_map import *
 from nilmtk import HDFDataStore, DataSet, TimeFrame, MeterGroup
 from nilmtk.disaggregate import CombinatorialOptimisation
+import warnings
+
 
 print ("Worked!")
