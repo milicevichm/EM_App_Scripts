45e6c5906fec4a8b2b28c9b8a2d124560856677c
Michael Milicevich <milicevichm@gmail.com>
2015-03-30 13:34:49 -0400

    checking string types

--- disag_script_v2.py
+++ disag_script_v2.py
@@ -27,14 +27,22 @@ warnings.filterwarnings("ignore")
 #[4]: Ending time/date: t2
 
 # Verify that the correct amount of input arguments have been entered
-if len(sys.argv) != 4:
-	sys.exit("Error: Incorrect amount of input arguments given. Script terminated.")
+#if len(sys.argv) != 4:
+#	sys.exit("Error: Incorrect amount of input arguments given. Script terminated.")
 
 #load arguments into script
-disag_appliance = sys.argv[1]
-t1 = int(sys.argv[2]) #+ " 00:00:00+00:00"
-t2 = int(sys.argv[3]) #+ " 00:00:00+00:00"
+#disag_appliance = sys.argv[1]
+#t1 = sys.argv[2] #+ " 00:00:00+00:00"
+#t2 = sys.argv[3] #+ " 00:00:00+00:00"
 
+t1_raw = "20110418-0922"
+t2_raw = "20110524-1557"
+
+t1 = parser(t1_raw)
+t2 = parser(t2_raw)
+
+print(t1)
+print(t2)
 # to add:
 #			1) load REDD data from database (SQL interface)*
 #
@@ -44,53 +52,53 @@ t2 = int(sys.argv[3]) #+ " 00:00:00+00:00"
 km = Key_Map(1)
 
 # verify a real appliance has been entered
-if km.is_in_map(disag_appliance) == False:
-	sys.exit("An incorrect appliance name has been entered. Please ensure the entered name is exactly correct.")
+#if km.is_in_map(disag_appliance) == False:
+#	sys.exit("An incorrect appliance name has been entered. Please ensure the entered name is exactly correct.")
 
-redd_data = DataSet("C:/NILM/Data/REDD/redd.h5")
+#redd_data = DataSet("C:/NILM/Data/REDD/redd.h5")
 
 # load mains of the building
-building_mains = redd_data.buildings[1].elec.mains()
+#building_mains = redd_data.buildings[1].elec.mains()
 
 #train disaggregation set
-co = CombinatorialOptimisation()
-training_set = redd_data.buildings[1].elec
-co.train(training_set)
+#co = CombinatorialOptimisation()
+#training_set = redd_data.buildings[1].elec
+#co.train(training_set)
 
 #set output datastore
-outputData = HDFDataStore("C:/NILM/Data/Output/output.h5",'w')
+#outputData = HDFDataStore("C:/NILM/Data/Output/output.h5",'w')
 
 
 #disaggregate
-co.disaggregate(building_mains,outputData)
+#co.disaggregate(building_mains,outputData)
 
-#set sub-datastore for CSV output
-output_csv_store = outputData.store.__getitem__(km.get_key(disag_appliance))
+# #set sub-datastore for CSV output
+# output_csv_store = outputData.store.__getitem__(km.get_key(disag_appliance))
 
-#set date parameters
-output_csv_store = output_csv_store[t1:t2]
+# #set date parameters
+# output_csv_store = output_csv_store[t1:t2]
 
-#fill NA values with 0 for graphing 
-output_csv_store = output_csv_store.fillna(value=0)
-
-
-#metrics processing ----------------------------------------------------------
-#create dict to hold energy metrics
-metrics_f1 = open('C:/NILM/Data/Metrics/f1.txt', 'w')
-metrics_mean_error = open('C:/NILM/Data/Metrics/mean_err.txt', 'w')
-metrics_rms_error = open('C:/NILM/Data/Metrics/rms_err.txt','w')
-metrics_disag_time = open('C:/NILM/Data/Metrics/disag_time.txt','w')
+# #fill NA values with 0 for graphing 
+# output_csv_store = output_csv_store.fillna(value=0)
 
 
+# #metrics processing ----------------------------------------------------------
+# #create dict to hold energy metrics
+# metrics_f1 = open('C:/NILM/Data/Metrics/f1.txt', 'w')
+# metrics_mean_error = open('C:/NILM/Data/Metrics/mean_err.txt', 'w')
+# metrics_rms_error = open('C:/NILM/Data/Metrics/rms_err.txt','w')
+# metrics_disag_time = open('C:/NILM/Data/Metrics/disag_time.txt','w')
 
-#output to csv
-output_csv = open('C:/NILM/Data/output/disag_output.csv','w')
-output_csv.write(output_csv_store.to_csv())
 
 
-#Close open datastores
-o_ds.store.close()
-redd_data.store.close()
-outputData.store.close()
+# #output to csv
+# output_csv = open('C:/NILM/Data/output/disag_output.csv','w')
+# output_csv.write(output_csv_store.to_csv())
+
+
+# #Close open datastores
+# o_ds.store.close()
+# redd_data.store.close()
+# outputData.store.close()
 
 
